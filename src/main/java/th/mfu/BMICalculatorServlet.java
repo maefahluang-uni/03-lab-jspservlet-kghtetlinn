26f20109e481d73fce7cd115847e4062faa522f6
kghtetlinn <122962925+kghtetlinn@users.noreply.github.com>
2023-09-05 08:56:42 +0000

    all done

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -9,20 +9,45 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 //TODO: add webservlet to "/calbmi"
-public class BMICalculatorServlet extends HttpServlet{
+@WebServlet("/calbmi")
+public class BMICalculatorServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //TODO: get parameter from request: "weight" and "height"
-        
-        //TODO: calculate bmi
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO: get parameter from request: "weight" and "height"
+        String weight = request.getParameter("weight");
+        String height = request.getParameter("height");
+        double weight1 = Double.parseDouble(weight);
+        double height1 = Double.parseDouble(height);
 
-        //TODO: determine the built from BMI
-      
-        //TODO: add bmi and built to the request's attribute
+        // TODO: calculate bmi
+        double BMI = weight1 / (height1 * height1);
+        int bmi = (int) Math.round(BMI);
+        // TODO: determine the built from BMI
+        String build;
+        if (bmi < 18.5) {
+            build = "underweight";
+        } else if (bmi < 25) {
+            build = "normal";
 
-        //TODO: forward to jsp
-           
+        } else if (bmi < 30) {
+            build = "overweight";
+
+        } else if (bmi < 35) {
+            build = "obese";
+
+        } else {
+            build = "extremely obese";
+        }
+
+        // TODO: add bmi and built to the request's attribute
+        request.setAttribute("BMI", bmi);
+        request.setAttribute("build", build);
+
+        // TODO: forward to jsp
+        request.getRequestDispatcher("bmi_result.jsp").forward(request, response);
+
     }
-    
+
 }
